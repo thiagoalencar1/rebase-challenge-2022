b5f6bc6e1451db03868d47c5c3dd61cdd85d2c41
Thiago Alencar <talencar@gmail.com>
2022-07-13 21:39:46 -0300

    Adiciona teste de criação de banco

--- tests/db_prepare.rb
+++ tests/db_prepare.rb
@@ -9,5 +9,15 @@ class Database < Test::Unit::TestCase
     assert_equal 'postgres', conn.db, 'Database connection successful'
     conn.close
   end
+
+  def test_table_create
+    conn = PG.connect(dbname: "postgres", host: '172.17.0.2', port: 5432, user: 'postgres', password: 'pass')
+    conn.exec_params("CREATE TABLE test_table(some_column INTEGER)")
+
+    assert_equal 'test_table', conn.exec("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'").first['table_name'], 'Table created'
+
+    conn.exec("DROP TABLE test_table")
+    conn.close
+  end
   
 end
